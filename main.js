1fe97d56b8f4d78f846f54dc149bd00f9f0f3d8c
Brayan Henrique <104778083+brayan9x@users.noreply.github.com>
2023-10-27 16:26:08 -0300

    ergd

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function TocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').OnClick=TocaSomPom;
+document.querySelector('.tecla_pom').onclick=TocaSomPom;
 
 function tocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
